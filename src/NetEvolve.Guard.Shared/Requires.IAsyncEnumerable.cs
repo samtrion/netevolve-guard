b3f9a6029a52e01ef345cb0a3150e6a8d2fe0a3b
Martin Stühmer <me@samtrion.net>
2021-11-05 15:15:02 +0100

    feat: Added Support for `IAsyncEnumerable` and `IBinaryNumber`

---
+++ src/NetEvolve.Guard.Shared/Requires.IAsyncEnumerable.cs
@@ -0,0 +1,73 @@
+#if NETSTANDARD2_1_OR_GREATER || NET5_0_OR_GREATER
+namespace NetEvolve.Guard;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+public static partial class Requires
+{
+  public static async ValueTask ItemsNotNullAsync<T>([NotNull] IAsyncEnumerable<T?> value, [CallerArgumentExpression("value")] string? parameterName = null) where T : class
+  {
+    if (await value.InternalAnyAsync(ValueIsNull))
+    {
+      throw new ArgumentException(null, parameterName);
+    }
+
+    static bool ValueIsNull([NotNullWhen(false)] T? value) => value is null;
+  }
+
+  public static async ValueTask ItemsNotNullOrEmptyAsync([NotNull] IAsyncEnumerable<string?> value, [CallerArgumentExpression("value")] string? parameterName = null)
+  {
+    if (await value.InternalAnyAsync(string.IsNullOrEmpty))
+    {
+      throw new ArgumentException(null, parameterName);
+    }
+  }
+
+  public static async ValueTask ItemsNotNullOrWhiteSpaceAsync([NotNull] IAsyncEnumerable<string?> value, [CallerArgumentExpression("value")] string? parameterName = null)
+  {
+    if (await value.InternalAnyAsync(string.IsNullOrWhiteSpace))
+    {
+      throw new ArgumentException(null, parameterName);
+    }
+  }
+
+  [return: NotNull]
+  public static async ValueTask NotNullOrEmptyAsync<T>([NotNull] IAsyncEnumerable<T>? value, [CallerArgumentExpression("value")] string? parameterName = null)
+  {
+    if (value is null)
+    {
+      throw new ArgumentNullException(parameterName);
+    }
+
+    if (!await value.InternalAnyAsync())
+    {
+      throw new ArgumentException(null, parameterName);
+    }
+  }
+
+  private static async ValueTask<bool> InternalAnyAsync<T>(this IAsyncEnumerable<T> value)
+  {
+    await using (var enumerator = value.GetAsyncEnumerator())
+    {
+      return await enumerator.MoveNextAsync();
+    }
+  }
+
+  private static async ValueTask<bool> InternalAnyAsync<T>(this IAsyncEnumerable<T> value, Func<T, bool> predicate)
+  {
+    await foreach (var item in value.ConfigureAwait(false))
+    {
+      if (predicate(item))
+      {
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
+#endif
---
+++ src/NetEvolve.Guard.Shared/Requires.IBinaryNumber.cs
@@ -0,0 +1,19 @@
+#if NET6_0_OR_GREATER && USE_GENERIC_MATH_FEATURE
+namespace NetEvolve.Guard;
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+
+public static partial class Requires
+{
+    [RequiresPreviewFeatures]
+    public static void NotPow2<T>(T value, [CallerArgumentExpression("value")] string? parameterName = null) where T : IBinaryNumber<T>
+    {
+        if (!T.IsPow2(value))
+        {
+            throw new ArgumentException(null, parameterName);
+        }
+    }
+}
+#endif
