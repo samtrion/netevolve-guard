0d89aa994950e57e231b4371996b0979f8424a0a
samtrion <3283596+samtrion@users.noreply.github.com>
2022-01-24 10:46:36 +0100

    chore: Added Tests for `IntPtr` and `UIntPtr`

---
+++ tests/NetEvolve.Guard.SharedTests/RequiresIntPtrTests.cs
@@ -0,0 +1,146 @@
+#if NET6_0_OR_GREATER && USE_GENERIC_MATH_FEATURE
+namespace NetEvolve.Guard.Tests;
+
+using NetEvolve.Guard;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+using CompareValue = System.IntPtr;
+
+[ExcludeFromCodeCoverage]
+public sealed class RequiresIntPtrTests
+{
+  private static CompareValue BaseValue { get; }
+  private static CompareValue MaxValue { get; } = CompareValue.MaxValue;
+  private static CompareValue MinValue { get; } = CompareValue.MinValue;
+
+  [Theory]
+  [MemberData(nameof(GetInBetweenData))]
+  public void InBetween_Theory_Expected(bool throwException, CompareValue value, CompareValue min, CompareValue max)
+  {
+    if (throwException)
+    {
+      _ = Assert.Throws<ArgumentOutOfRangeException>(nameof(value), () => Requires.InBetween(value, min, max));
+    }
+    else
+    {
+      Requires.InBetween(value, min, max);
+    }
+  }
+
+  [Theory]
+  [MemberData(nameof(GetNotBetweenData))]
+  public void NotBetween_Theory_Expected(bool throwException, CompareValue value, CompareValue min, CompareValue max)
+  {
+    if (throwException)
+    {
+      _ = Assert.Throws<ArgumentOutOfRangeException>(nameof(value), () => Requires.NotBetween(value, min, max));
+    }
+    else
+    {
+      Requires.NotBetween(value, min, max);
+    }
+  }
+
+  [Theory]
+  [MemberData(nameof(GetGreaterThanData))]
+  public void GreaterThan_Theory_Expected(bool throwException, CompareValue value, CompareValue compareValue)
+  {
+    if (throwException)
+    {
+      _ = Assert.Throws<ArgumentOutOfRangeException>(nameof(value), () => Requires.GreaterThan(value, compareValue));
+    }
+    else
+    {
+      Requires.GreaterThan(value, compareValue);
+    }
+  }
+
+  [Theory]
+  [MemberData(nameof(GetGreaterThanOrEqualData))]
+  public void GreaterThanOrEqual_Theory_Expected(bool throwException, CompareValue value, CompareValue compareValue)
+  {
+    if (throwException)
+    {
+      _ = Assert.Throws<ArgumentOutOfRangeException>(nameof(value), () => Requires.GreaterThanOrEqual(value, compareValue));
+    }
+    else
+    {
+      Requires.GreaterThanOrEqual(value, compareValue);
+    }
+  }
+
+  [Theory]
+  [MemberData(nameof(GetLessThanData))]
+  public void LessThan_Theory_Expected(bool throwException, CompareValue value, CompareValue compareValue)
+  {
+    if (throwException)
+    {
+      _ = Assert.Throws<ArgumentOutOfRangeException>(nameof(value), () => Requires.LessThan(value, compareValue));
+    }
+    else
+    {
+      Requires.LessThan(value, compareValue);
+    }
+  }
+
+  [Theory]
+  [MemberData(nameof(GetLessThanOrEqualData))]
+  public void LessThanOrEqual_Theory_Expected(bool throwException, CompareValue value, CompareValue compareValue)
+  {
+    if (throwException)
+    {
+      _ = Assert.Throws<ArgumentOutOfRangeException>(nameof(value), () => Requires.LessThanOrEqual(value, compareValue));
+    }
+    else
+    {
+      Requires.LessThanOrEqual(value, compareValue);
+    }
+  }
+
+  public static TheoryData GetInBetweenData => new TheoryData<bool, CompareValue, CompareValue, CompareValue>
+    {
+      { true, MinValue, BaseValue, MaxValue },
+      { true, MaxValue, BaseValue, MinValue },
+      { false, BaseValue, MinValue, MaxValue },
+      { false, BaseValue, MaxValue, MinValue }
+    };
+
+  public static TheoryData GetNotBetweenData => new TheoryData<bool, CompareValue, CompareValue, CompareValue>
+    {
+      { false, MinValue, BaseValue, MaxValue },
+      { false, MaxValue, BaseValue, MinValue },
+      { true, BaseValue, MinValue, MaxValue },
+      { true, BaseValue, MaxValue, MinValue }
+    };
+
+  public static TheoryData GetGreaterThanData => new TheoryData<bool, CompareValue, CompareValue>
+    {
+      { true, BaseValue, MaxValue },
+      { true, BaseValue, BaseValue },
+      { false, BaseValue, MinValue }
+    };
+
+  public static TheoryData GetGreaterThanOrEqualData => new TheoryData<bool, CompareValue, CompareValue>
+    {
+      { true, BaseValue, MaxValue },
+      { false, BaseValue, BaseValue },
+      { false, BaseValue, MinValue }
+    };
+
+  public static TheoryData GetLessThanData => new TheoryData<bool, CompareValue, CompareValue>
+    {
+      { true, BaseValue, MinValue },
+      { true, BaseValue, BaseValue },
+      { false, BaseValue, MaxValue }
+    };
+
+  public static TheoryData GetLessThanOrEqualData => new TheoryData<bool, CompareValue, CompareValue>
+    {
+      { true, BaseValue, MinValue },
+      { false, BaseValue, BaseValue },
+      { false, BaseValue, MaxValue }
+    };
+}
+
+#endif
